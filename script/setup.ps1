d99b2c63f1b4e2247071ec5b353ed5806f61a78b
James Pogran <james.pogran@outlook.com>
2017-07-10 14:37:08 -0400

    cleaup powershell setup file

--- script/setup.ps1
+++ script/setup.ps1
@@ -5,32 +5,22 @@ $parent = Split-Path -Path $PSScriptRoot
 
 $files = Get-ChildItem -Path $parent -Force -Exclude '.git','*.md','scripts'
 $files | % {
-  $name = $_.Name
+  $name     = $_.Name
   $fullname = $_.FullName
   switch($name){
     '.gitconfig-osx'{
       if([System.Environment]::OSVersion.Platform -eq 'Unix'){
         $linkDir = "$($HOME)/.gitconfig-shell"
-      }else{
-        $linkDir = $null
-      }
+      }else{ $linkDir = $null }
     }
     '.gitconfig-windows'{
       if([System.Environment]::OSVersion.Platform -eq 'Windows'){
         $linkDir = "$($HOME)/.gitconfig-shell"
-      }else{
-        $linkDir = $null
-      }
+      }else{ $linkDir = $null }
     }
-    'vscode.settings.json'{
-      $linkDir = "$($HOME)/Library/Application Support/Code/User/settings.json"
-    }
-    'profile.ps1'{
-      $linkDir = '~/.config/powershell/profile.ps1'
-    }
-    default{
-      $linkDir =  "$($HOME)/$($name)"
-    }
+    'vscode.settings.json'{ $linkDir = "$($HOME)/Library/Application Support/Code/User/settings.json" }
+    'profile.ps1'{ $linkDir = '~/.config/powershell/profile.ps1' }
+    default{ $linkDir =  "$($HOME)/$($name)" }
   }
   if($linkDir){
     Write-Verbose "Linking $($fullname) to $($linkDir)"
