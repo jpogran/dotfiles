c48b21a440e7bf650a4c45dcac1be8c7a807bd16
James Pogran <james.pogran@outlook.com>
2017-09-18 13:43:59 -0400

    updates

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,7 +1,17 @@
+$env:SSH_KEY_PATH = "$HOME/.ssh/id_rsa"
+$env:BEAKER_keyfile="$HOME/.ssh/id_rsa-acceptance"
+$env:BEAKER_destroy="always"
+$env:BEAKER_debug=true
+$env:PUPPET_INSTALL_TYPE="agent"
+$env:INSTALLATION_TYPE="git"
+$env:BUNDLE_PATH=".bundle/gems"
+$env:BUNDLE_BIN=".bundle/bin"
+
 Set-Alias -Name time  -Value Measure-Command
 Set-Alias -Name ss    -Value Select-String
 Set-Alias -Name count -Value Measure-Object
 Set-Alias -Name ll    -Value Get-ChildItem
+Set-Alias -Name git   -Value hub
 
 function .. { Set-Location -Path .. }
 function la { Get-ChildItem -Force }
