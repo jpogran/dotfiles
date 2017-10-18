a2a6b5da550961871ed30835544938a9589fa6f7
James Pogran <james.pogran@puppet.com>
2017-10-18 10:58:12 -0400

    adding setup script

--- script/osx.sh
+++ script/osx.sh
@@ -105,3 +105,24 @@ echo "Some changes will not take effect until you reboot your machine."
 
 # reset to default dock
 # defaults delete com.apple.dock
+
+if [ ! -f ~/.iterm/themes/SolarizedDark.itermcolors ]; then
+    # install SolarizedDark
+    mkdir -p ~/.iterm/themes
+    wget -O ~/.iterm/themes/SolarizedDark.itermcolors https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/Solarized%20Dark.itermcolors
+    open ~/.iterm/themes/SolarizedDark.itermcolors
+fi
+
+# Don’t display the annoying prompt when quitting iTerm
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+if [ ! -f ~/src/third/powerline-fonts ]; then
+    git clone https://github.com/powerline/fonts.git ~/src/third/powerline-fonts
+    cd ~/src/third/powerline-fonts
+    ./install.sh
+    cd ..
+fi
+
+rbenv install 2.4.1 -s
--- script/setup.sh
+++ script/setup.sh
@@ -1,21 +1,14 @@
+#!/usr/bin/env bash
 
-if [ ! -f ~/.iterm/themes/SolarizedDark.itermcolors ]; then
-    # install SolarizedDark
-    mkdir -p ~/.iterm/themes
-    wget -O ~/.iterm/themes/SolarizedDark.itermcolors https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/Solarized%20Dark.itermcolors
-    open ~/.iterm/themes/SolarizedDark.itermcolors
-fi
+set -e
 
-# Don’t display the annoying prompt when quitting iTerm
-defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+CONFIG="install.conf.yaml"
+DOTBOT_DIR="dotbot"
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+DOTBOT_BIN="bin/dotbot"
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if [ ! -f ~/src/third/powerline-fonts ]; then
-    git clone https://github.com/powerline/fonts.git ~/src/third/powerline-fonts
-    cd ~/src/third/powerline-fonts
-    ./install.sh
-    cd ..
-fi
+cd "${BASEDIR}"
+git submodule update --init --recursive "${DOTBOT_DIR}"
 
-rbenv install 2.4.1 -s
+"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
