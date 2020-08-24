f2cb6e875feead695945d82eed16bdd8ea8dc7a7
James Pogran <jpogran@outlook.com>
2020-08-23 22:33:03 -0400

    installer

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,8 +1,19 @@
+Copy-Item -Force -Path (Join-Path $PSScriptRoot "git/*") -Destination ${HOME}
+Copy-Item -Force -Path (Join-Path $PSScriptRoot "ruby/*") -Destination ${HOME}
+Copy-Item -Force -Path (Join-Path $PSScriptRoot "vim/*") -Destination ${HOME}
+Copy-Item -Force -Path (Join-Path $PSScriptRoot "zsh/*") -Destination ${HOME}
 
-Copy-Item -Recurse -Force git ${HOME}
-Copy-Item -Recurse -Force ruby ${HOME}
-Copy-Item -Recurse -Force vim ${HOME}
-Copy-Item -Recurse -Force zsh ${HOME}
+switch ($PSVersionTable.PSVersion.Major) {
+  7 {
+    New-Item -itemType Directory -Force -Path (Join-Path ${HOME} "/Documents/PowerShell") | Out-Null
+    Copy-Item -Force -Path (Join-Path $PSScriptRoot "pwsh/pwsh.ps1") -Destination (Join-Path ${HOME} "/Documents/PowerShell/Microsoft.PowerShell_profile.ps1")
+  }
+  5 {
+    New-Item -itemType Directory -Force -Path (Join-Path ${HOME} "/Documents/WindowsPowerShell") | Out-Null
+    Copy-Item -Force -Path (Join-Path $PSScriptRoot "pwsh/pwsh.ps1") -Destination (Join-Path ${HOME} "/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1")
+  }
+}
 
-Copy-Item -Path vscode/settings.json -Destination c:/users/james/appdata/roaming/code/user/settings.json
-Copy-Item -Path vscode/keybindings.json -Destination c:/users/james/appdata/roaming/code/user/keybindings.json
+New-Item -itemType Directory -Force -Path (Join-Path $env:APPDATA "/Code/User") | Out-Null
+Copy-Item -Force -Path (Join-Path $PSScriptRoot "vscode/settings.json") -Destination (Join-Path $env:APPDATA "/code/user/settings.json")
+Copy-Item -Force -Path (Join-Path $PSScriptRoot "vscode/keybindings.json") -Destination (Join-Path $env:APPDATA "/code/user/keybindings.json")
