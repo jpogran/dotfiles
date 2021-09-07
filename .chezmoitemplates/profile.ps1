aa74bfbe4fd02df97cf8baaa7df3b6f2fa0582fc
James Pogran <jpogran@outlook.com>
2021-09-07 16:10:19 -0400

    updates

--- .chezmoitemplates/profile.ps1
+++ .chezmoitemplates/profile.ps1
@@ -1,3 +1,10 @@
+if ($host.Name -eq 'ConsoleHost') {
+  Import-Module PSReadLine
+}
+Import-Module Terminal-Icons
+Import-Module posh-git
+
+$env:Path = "C:\Users\james\bin;" + $env:Path
 $env:StarShell = 'pwsh-' + $PSVersionTable.PSVersion.ToString()
 
 Set-Alias -Name count -Value Measure-Object
@@ -9,6 +16,16 @@ function .. { Set-Location -Path .. }
 function la { Get-ChildItem -Force }
 function be { bundle exec $args }
 function bi { bundle install $args }
+
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+  param($wordToComplete, $commandAst, $cursorPosition)
+  [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+  $Local:word = $wordToComplete.Replace('"', '""')
+  $Local:ast = $commandAst.ToString().Replace('"', '""')
+  winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+  }
+}
 
 $PSDefaultParameterValues['ConvertTo-Csv:NoTypeInformation'] = $true
 $PSDefaultParameterValues['Export-Csv:NoTypeInformation'] = $true
@@ -32,7 +49,10 @@ if ($env:WT_SESSION) {
   Set-PSReadLineKeyHandler -Chord Ctrl+h -Function BackwardDeleteWord
 }
 
-Invoke-Expression (&starship init powershell)
+# Invoke-Expression (&starship init powershell)
+# oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\jandedobbeleer.omp.json | Invoke-Expression
+# oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\powerlevel10k_rainbow.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\agnosterplus.omp.json | Invoke-Expression
 
 if ($env:STARSHIP_SHELL -eq 'powershell') {
   Set-PSReadLineOption -prompttext "`e[1;32m❯ ", '❯ '
