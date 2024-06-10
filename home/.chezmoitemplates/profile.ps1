396e2910bc35a9266fedb821065add772f9f26a8
James Pogran <jpogran@outlook.com>
2024-06-10 12:18:51 -0400

    Update home/.chezmoitemplates/gh/config.yml Add home/.chezmoitemplates/gh/hosts.yml Update home/.chezmoitemplates/profile.ps1 Update home/.zshrc Update home/.config/git/config

--- home/.chezmoitemplates/profile.ps1
+++ home/.chezmoitemplates/profile.ps1
@@ -3,7 +3,7 @@ if ($host.Name -eq 'ConsoleHost') {
 }
 Import-Module posh-git
 
-$env:EDITOR    = "code -w"
+$env:EDITOR = "code -w"
 
 Set-Alias -Name count -Value Measure-Object
 Set-Alias -Name ll    -Value Get-ChildItem
@@ -13,25 +13,15 @@ Set-Alias -Name time  -Value Measure-Command
 function .. { Set-Location -Path .. }
 function la { Get-ChildItem -Force }
 
-Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
-  param($wordToComplete, $commandAst, $cursorPosition)
-  [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
-  $Local:word = $wordToComplete.Replace('"', '""')
-  $Local:ast = $commandAst.ToString().Replace('"', '""')
-  winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-  }
-}
-
-$PSDefaultParameterValues['ConvertTo-Csv:NoTypeInformation']   = $true
-$PSDefaultParameterValues['Export-Csv:NoTypeInformation']      = $true
-$PSDefaultParameterValues['Install-Module:AllowClobber']       = $true
-$PSDefaultParameterValues['Install-Module:Force']              = $true
+$PSDefaultParameterValues['ConvertTo-Csv:NoTypeInformation'] = $true
+$PSDefaultParameterValues['Export-Csv:NoTypeInformation'] = $true
+$PSDefaultParameterValues['Install-Module:AllowClobber'] = $true
+$PSDefaultParameterValues['Install-Module:Force'] = $true
 $PSDefaultParameterValues['Install-Module:SkipPublisherCheck'] = $true
-$PSDefaultParameterValues['Install-Module:Scope']              = 'CurrentUser'
-$PSDefaultParameterValues['Install-Package:Repository']        = 'PSGallery'
-$PSDefaultParameterValues['Out-Default:OutVariable']           = 'LastResult'
-$PSDefaultParameterValues['Out-File:Encoding']                 = 'utf8'
+$PSDefaultParameterValues['Install-Module:Scope'] = 'CurrentUser'
+$PSDefaultParameterValues['Install-Package:Repository'] = 'PSGallery'
+$PSDefaultParameterValues['Out-Default:OutVariable'] = 'LastResult'
+$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
 
 $ExecutionContext.SessionState.InvokeCommand.LocationChangedAction += {
   [Environment]::CurrentDirectory = $ExecutionContext.SessionState.Path.CurrentFileSystemLocation
