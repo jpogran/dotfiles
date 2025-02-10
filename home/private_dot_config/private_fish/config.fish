6a611e353181ea7d655b712c152e86f4f35f5f77
James Pogran <jpogran@outlook.com>
2025-02-10 13:59:15 -0500

    Update home/.config/fish/config.fish

--- home/private_dot_config/private_fish/config.fish
+++ home/private_dot_config/private_fish/config.fish
@@ -1,16 +1,26 @@
+#!/usr/bin/env fish
+
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 
 set -U fish_greeting
 
+set -x EDITOR 'code --wait'
+
 # Add the shims to the end of PATH as fallback for VS Code
 # https://mise.jdx.dev/ide-integration.html#vscode
 # set -x PATH $PATH ~/.local/share/mise/shims
+# set -x PATH $PATH ~/Applications/bin ~/.npm/bin ~/.cargo/bin
 
-set -x PATH $PATH ~/Applications/bin ~/.npm/bin ~/.cargo/bin
+fish_add_path /opt/homebrew/opt/libpq/bin
+set -gx LDFLAGS "-L/opt/homebrew/opt/libpq/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/libpq/include"
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    eval "$(tfcdev rc)"
+    # echo "$(doormat login && doormat artifactory create-token | jq -r '.access_token')" \
+    #   | docker login -u "${USER}@hashicorp.com" --password-stdin docker.artifactory.hashicorp.engineering
+
     mise activate fish | source
 
     /opt/homebrew/bin/brew shellenv | source
