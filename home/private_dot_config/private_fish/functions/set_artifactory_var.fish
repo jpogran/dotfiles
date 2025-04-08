00c22ee156a9dd3e7ddd1ee0ac7e135d071c2c08
James Pogran <jpogran@outlook.com>
2025-04-07 20:12:21 -0400

    Update home/.config/fish/config.fish Update home/.config/fish/functions/set_artifactory_var.fish

--- home/private_dot_config/private_fish/functions/set_artifactory_var.fish
+++ home/private_dot_config/private_fish/functions/set_artifactory_var.fish
@@ -1,6 +1,11 @@
 function set_artifactory_var
     while true
-        set -Ux BUNDLE_ARTIFACTORY__HASHICORP__ENGINEERING "james.pogran%40hashicorp.com:"(doormat login && doormat artifactory create-token | jq -r '.access_token')
+        set token (doormat login && doormat artifactory create-token | jq -r '.access_token')
+
+        set -Ux BUNDLE_ARTIFACTORY__HASHICORP__ENGINEERING "james.pogran%40hashicorp.com:$token"
+
+        docker login -u "james.pogran @hashicorp.com" -p $token docker.artifactory.hashicorp.engineering
+
         sleep 1800
     end
 end
