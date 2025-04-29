87b3deda562276613c0e2364df7203a5e5ab289b
James Pogran <jpogran@outlook.com>
2025-04-29 10:23:14 -0400

    Update home/.config/fish/functions/set_atlas_env.fish

--- home/private_dot_config/private_fish/functions/set_atlas_env.fish
+++ home/private_dot_config/private_fish/functions/set_atlas_env.fish
@@ -1,8 +1,6 @@
 function set_atlas_env
     set -gx GITHUB_TOKEN "$(op read 'op://private/atlas-github-token/token')"
-    if not set -q ACCESS_TOKEN
-        set ACCESS_TOKEN "$(doormat login && doormat artifactory create-token | jq -r .access_token)"
-    end
+    set ACCESS_TOKEN "$(doormat login && doormat artifactory create-token | jq -r .access_token)"
     set -gx ATLAS_JANUS_ENGINE_PATH ~/src/hashicorp/janus-rails-engine
     set -gx USER "james.pogran"
     set -gx BUNDLE_ARTIFACTORY__HASHICORP__ENGINEERING "$USER%40hashicorp.com:$ACCESS_TOKEN"
