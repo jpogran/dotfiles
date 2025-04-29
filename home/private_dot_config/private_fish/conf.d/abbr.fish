a0fa4c335468b5c1972a20d94ee81640230eb249
James Pogran <jpogran@outlook.com>
2025-04-29 10:15:13 -0400

    Update .vscode/settings.json Update home/.config/fish/conf.d/abbr.fish Update home/.config/fish/config.fish Add home/.config/fish/functions/set_atlas_env.fish Add home/.config/fish/functions/set_xdg_basedirs.fish

--- home/private_dot_config/private_fish/conf.d/abbr.fish
+++ home/private_dot_config/private_fish/conf.d/abbr.fish
@@ -1,3 +1,33 @@
 abbr cls clear
 abbr cd.. cd ..
 abbr .. cd ..
+
+# golang
+abbr -a -- gob 'go build'
+abbr -a -- goc 'go clean'
+abbr -a -- god 'go doc'
+abbr -a -- gof 'go fmt'
+abbr -a -- gofa 'go fmt ./...'
+abbr -a -- gog 'go get'
+abbr -a -- goi 'go install'
+abbr -a -- gol 'go list'
+abbr -a -- gop 'cd $GOPATH'
+abbr -a -- gopb 'cd $GOPATH/bin'
+abbr -a -- gops 'cd $GOPATH/src'
+abbr -a -- gor 'go run'
+abbr -a -- got 'go test'
+abbr -a -- gov 'go vet'
+
+# ruby
+abbr -a -- be 'bundle exec'
+abbr -a -- ber 'bundle exec rspec'
+abbr -a -- bi 'bundle install'
+abbr -a -- bu 'bundle update'
+abbr -a -- rake 'bundle exec rake'
+abbr -a -- rs 'rails server'
+abbr -a -- rc 'rails console'
+abbr -a -- rg 'rails generate'
+abbr -a -- rd 'rails db:migrate'
+abbr -a -- rdb 'rails db:rollback'
+abbr -a -- rds 'rails db:seed'
+abbr -a -- rdm 'rails db:migrate && rails db:seed'
---
+++ home/private_dot_config/private_fish/functions/set_atlas_env.fish
@@ -0,0 +1,12 @@
+function set_atlas_env
+    set -gx GITHUB_TOKEN (op read "op://private/atlas-github-token/token")
+    if not set -q ACCESS_TOKEN
+        set ACCESS_TOKEN $(doormat artifactory create-token | jq -r .access_token)
+    end
+    set -gx ATLAS_JANUS_ENGINE_PATH ~/src/hashicorp/janus-rails-engine
+    set -gx USER "james.pogran"
+    set -gx BUNDLE_ARTIFACTORY__HASHICORP__ENGINEERING "$USER%40hashicorp.com:$ACCESS_TOKEN"
+    echo $ACCESS_TOKEN | docker login docker.artifactory.hashicorp.engineering --username "$USER@hashicorp.com" --password-stdin
+    echo $ACCESS_TOKEN | docker login tf-cloud-local.artifactory.hashicorp.engineering --username "$USER@hashicorp.com" --password-stdin
+    eval "$(tfcdev rc)"
+end
