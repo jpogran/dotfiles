05892a3a8bc4b54827cef653e905f3bcf47625d1
James Pogran <jpogran@outlook.com>
2025-05-08 12:08:53 -0400

    fish

--- home/private_dot_config/private_fish/conf.d/00-env.fish
+++ home/private_dot_config/private_fish/conf.d/00-env.fish
@@ -1,3 +1,17 @@
+set -gx HOMEBREW_PREFIX "/opt/homebrew"
+
+fish_add_path "$HOMEBREW_PREFIX/bin"
+fish_add_path "$HOMEBREW_PREFIX/sbin"
+fish_add_path "$HOMEBREW_PREFIX/opt/libpq/bin"
+
+# Add homebrew completions
+if test -e "$HOMEBREW_PREFIX/share/fish/completions"
+    set -a fish_complete_path "$HOMEBREW_PREFIX/share/fish/completions"
+end
+
+# Other homebrew vars.
+set -gx HOMEBREW_NO_ANALYTICS 1
+
 # Set XDG basedirs.
 # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 set -q XDG_CONFIG_HOME; or set -Ux XDG_CONFIG_HOME $HOME/.config
@@ -19,6 +33,11 @@ end
 
 # Ensure manpath is set to something so we can add to it.
 set -q MANPATH; or set -gx MANPATH ''
+
+# Add more man page paths.
+for manpath in (path filter $__fish_data_dir/man /usr/local/share/man /usr/share/man)
+    set -a MANPATH $manpath
+end
 
 # Set initial working directory.
 set -g IWD $PWD
